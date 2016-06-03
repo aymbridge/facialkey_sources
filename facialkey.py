e16a115b09d76bc9f2d60fe1886e377db700638a
aymbridge <aymbreil@yahoo.fr>
2016-06-03 15:31:48 +0200

    simple_perceptron
    added neural net and training

--- facialkey.py
+++ facialkey.py
@@ -1,18 +1,21 @@
 """ Facial Keypoints detection form kaggle """
 
-
 import os 
+import time
 
 import numpy as np 
 import theano
-import theano.tensor as T 
+import theano.tensor as T
+
 import lasagne 
+from lasagne import layers
+from lasagne.updates import nesterov_momentum
 
 from pandas.io.parsers import read_csv
 from sklearn.utils import shuffle 
 
-FTRAIN = '~/Documents/Machine_learning/facialkeypoints/training.csv'
-FTEST = '~/Documents/Machine_learning/facialkeypoints/test.csv'
+FTRAIN = '~/Documents/Machine_learning/facialkeypoints/datasets/training.csv'
+FTEST = '~/Documents/Machine_learning/facialkeypoints/datasets/test.csv'
 
 
 def load(test = False, cols = None):
@@ -41,10 +44,155 @@ def load(test = False, cols = None):
 	else:
 		y = None
 
-	return X, y
+	#Split data for training and testing(20%) 
+	#X_train, y_train, X_val, y_val, X_test, y_test
+	X_train = X[:1720,:]
+	y_train = y[:1720,:]
+
+	X_val = X[1720:,:] 
+	y_val = y[1720:,:]
+
+	return X_train, y_train, X_val, y_val
+
+
+def build_simplelp(input_var=None):
+    # This creates an simple perceptron of one hidden layer
+
+    # Input layer, specifying the expected input shape of the network
+    # (unspecified batchsize, 1 channel, 28 rows and 28 columns) and
+    # linking it to the given Theano variable `input_var`, if any:
+    l_in = lasagne.layers.InputLayer(shape=(None, 9216), input_var=input_var)
+
+    # Apply 20% dropout to the input data:
+    l_in_drop = lasagne.layers.DropoutLayer(l_in, p=0.2)
+
+    # Add a fully-connected layer of 800 units, using the linear rectifier, and
+    # initializing weights with Glorot's scheme (which is the default anyway):
+    l_hid1 = lasagne.layers.DenseLayer(l_in_drop, num_units=100, nonlinearity=lasagne.nonlinearities.rectify, W=lasagne.init.GlorotUniform())
+
+    # We'll now add dropout of 50%:
+    l_hid1_drop = lasagne.layers.DropoutLayer(l_hid1, p=0.5)
+
+    # Finally, we'll add the fully-connected output layer of 30 units:
+    l_out = lasagne.layers.DenseLayer(l_hid1_drop, num_units=30, nonlinearity=None)
+
+    # Each layer is linked to its incoming layer(s), so we only need to pass
+    # the output layer to give access to a network in Lasagne:
+    return l_out
+
+
+################# Batch iterator ###################
+def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
+    assert len(inputs) == len(targets)
+    if shuffle:
+        indices = np.arange(len(inputs))
+        np.random.shuffle(indices)
+    for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
+        if shuffle:
+            excerpt = indices[start_idx:start_idx + batchsize]
+        else:
+            excerpt = slice(start_idx, start_idx + batchsize)
+        yield inputs[excerpt], targets[excerpt]
+
+
+def main(num_epochs=500):
+	# Load the dataset
+    print("Loading data...")
+    X_train, y_train, X_val, y_val = load()
+
+	#print("X.shape == {}; X.min == {:.3f}; X.max == {:.3f}".format(X.shape, X.min(), X.max()))
+	#print("y.shape == {}; y.min == {:.3f}; y.max == {:.3f}".format(y.shape, y.min(), y.max()))
+
+    # Prepare Theano variables for inputs and targets
+    input_var = T.fmatrix('inputs')
+    target_var = T.fmatrix('targets')
+
+    # Create neural network model
+    print("Building model and compiling functions...")
+    network = build_simplelp(input_var)
+
+    # Create a loss expression for training
+    prediction = lasagne.layers.get_output(network)
+    loss = lasagne.objectives.squared_error(prediction, target_var)
+    loss = loss.mean()
+
+    # We could add some weight decay as well here, see lasagne.regularization.
+
+    # Create update expressions for training
+    params = lasagne.layers.get_all_params(network, trainable=True)
+    updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=0.01, momentum=0.9)
+
+    # Create a loss expression for validation/testing. The crucial difference
+    # here is that we do a deterministic forward pass through the network,
+    # disabling dropout layers.
+    test_prediction = lasagne.layers.get_output(network, deterministic=True)
+    test_loss = lasagne.objectives.squared_error(test_prediction,target_var)
+    test_loss = test_loss.mean()
+
+    # As a bonus, also create an expression for the classification accuracy:
+    #test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var), dtype=theano.config.floatX)
+
+    # Compile a function performing a training step on a mini-batch (by giving
+    # the updates dictionary) and returning the corresponding training loss:
+    train_fn = theano.function([input_var, target_var], loss, updates=updates)
+
+    # Compile a second function computing the validation loss and accuracy:
+    #val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
+    val_fn = theano.function([input_var, target_var], test_loss)
+
+
+   # Launch the training loop.
+    print("Starting training...")
+    # We iterate over epochs:
+    for epoch in range(num_epochs):
+        # In each epoch, we do a full pass over the training data:
+        train_err = 0
+        train_batches = 0
+        start_time = time.time()
+        for batch in iterate_minibatches(X_train, y_train, 200, shuffle=True):
+            inputs, targets = batch
+            train_err += train_fn(inputs, targets)
+            train_batches += 1
+
+        # And a full pass over the validation data:
+        val_err = 0
+        val_acc = 0
+        val_batches = 0
+        for batch in iterate_minibatches(X_val, y_val, 200, shuffle=False):
+            inputs, targets = batch
+            #err, acc = val_fn(inputs, targets)
+            err = val_fn(inputs, targets)
+            val_err += err
+            #val_acc += acc
+            val_batches += 1
+
+        # Then we print the results for this epoch:
+        print("Epoch {} of {} took {:.3f}s".format(epoch + 1, num_epochs, time.time() - start_time))
+        print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
+        print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
+        #print("  validation accuracy:\t\t{:.2f} %".format(val_acc / val_batches * 100))
+
+    #Save all network parameters after training
+    np.savez('model_simple_preceptron.npz', *lasagne.layers.get_all_param_values(network))
+
+    # # After training, we compute and print the test error:
+    # test_err = 0
+    # test_acc = 0
+    # test_batches = 0
+    # for batch in iterate_minibatches(X_test, y_test, 500, shuffle=False):
+    #     inputs, targets = batch
+    #     err, acc = val_fn(inputs, targets)
+    #     test_err += err
+    #     test_acc += acc
+    #     test_batches += 1
+    # print("Final results:")
+    # print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
+    # print("  test accuracy:\t\t{:.2f} %".format(
+    #     test_acc / test_batches * 100))
 
 if __name__ == '__main__':
-	X, y = load()
-	print("X.shape == {}; X.min == {:.3f}; X.max == {:.3f}".format(X.shape, X.min(), X.max()))
-	print("y.shape == {}; y.min == {:.3f}; y.max == {:.3f}".format(y.shape, y.min(), y.max()))
+	main(400)
 	
+
+
+
